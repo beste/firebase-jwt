e10a4c7f025eaedbad0b58dd6a60c76bd5354848
Jérôme Gamez <jerome@gamez.name>
2026-05-28 17:57:34 +0200

    Refactor tooling setup and composer scripts
    Adapting https://github.com/jeromegamez/cookiecutter-php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,10 +2,23 @@
 
 declare(strict_types=1);
 
-use Beste\PhpCsFixer\Config;
+$finder = PhpCsFixer\Finder::create()->in(__DIR__);
 
-$config = Config\Factory::fromRuleSet(new Config\RuleSet\Php81());
-
-$config->getFinder()->in(__DIR__);
-
-return $config;
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(
+        PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect()
+    )
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PER-CS3x0' => true,
+        'declare_strict_types' => true,
+        'multiline_whitespace_before_semicolons' => [
+            'strategy' => 'new_line_for_chained_calls',
+        ],
+        'no_unused_imports' => true,
+        'php_unit_internal_class' => [
+            'types' => ['abstract', 'final', 'normal'],
+        ],
+        'trim_array_spaces' => true,
+    ])
+    ->setFinder($finder);
